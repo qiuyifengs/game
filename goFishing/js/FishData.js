812c936c2fab85fa61517474cca890ff7bd19093
邱毅锋 <184899881@qq.com>
2017-03-22 10:34:15 +0800

    add goFishing game

---
+++ goFishing/js/FishData.js
@@ -0,0 +1,149 @@
+/* 
+* @Author: anchen
+* @Date:   2017-02-22 09:50:41
+* @Last Modified by:   anchen
+* @Last Modified time: 2017-03-07 11:28:05
+*/
+
+        var fishArr = [{
+            name:'小丑鱼_左',
+            fishType:' one_left',
+            AniType:' ani_left_one',
+            val:1
+        },{
+            name:'小丑鱼_左',
+            fishType:' one_left',
+            AniType:' ani_left_two',
+            val:1
+        },{
+            name:'小丑鱼_右',
+            fishType:' one_right',
+            AniType:' ani_right_one',
+            val:1
+        },{
+            name:'小丑鱼_右',
+            fishType:' one_right',
+            AniType:' ani_right_two',
+            val:1
+        },{
+            name:'黄斑鱼_左',
+            fishType:' two_left',
+            AniType:' ani_left_one',
+            val:2
+        },{
+            name:'黄斑鱼_左',
+            fishType:' two_left',
+            AniType:' ani_left_two',
+            val:2
+        },{
+            name:'黄斑鱼_右',
+            fishType:' two_right',
+            AniType:' ani_right_one',
+            val:2
+        },{
+            name:'黄斑鱼_右',
+            fishType:' two_right',
+            AniType:' ani_right_two',
+            val:2
+        },{
+            name:'透明鱼_左',
+            fishType:' three_left',
+            AniType:' ani_left_one',
+            val:3
+        },{
+            name:'透明鱼_左',
+            fishType:' three_left',
+            AniType:' ani_left_two',
+            val:3
+        },{
+            name:'透明鱼_右',
+            fishType:' three_right',
+            AniType:' ani_right_one',
+            val:3
+        },{
+            name:'透明鱼_右',
+            fishType:' three_right',
+            AniType:' ani_right_two',
+            val:3
+        },{
+            name:'长嘴鱼_左',
+            fishType:' four_left',
+            AniType:' ani_left_one',
+            val:4
+        },{
+            name:'长嘴鱼_左',
+            fishType:' four_left',
+            AniType:' ani_left_two',
+            val:4
+        },{
+            name:'长嘴鱼_右',
+            fishType:' four_right',
+            AniType:' ani_right_one',
+            val:4
+        },{
+            name:'长嘴鱼_右',
+            fishType:' four_right',
+            AniType:' ani_right_two',
+            val:4
+        },{
+            name:'花鱼_左',
+            fishType:' five_left',
+            AniType:' ani_left_one',
+            val:5
+        },{
+            name:'花鱼_左',
+            fishType:' five_left',
+            AniType:' ani_left_two',
+            val:5
+        },{
+            name:'花鱼_右',
+            fishType:' five_right',
+            AniType:' ani_right_one',
+            val:5
+        },{
+            name:'花鱼_右',
+            fishType:' five_right',
+            AniType:' ani_right_two',
+            val:5
+        },{
+            name:'鲸鱼_左',
+            fishType:' sever_left',
+            AniType:' ani_left_one',
+            val:7
+        },{
+            name:'鲸鱼_左',
+            fishType:' sever_left',
+            AniType:' ani_left_two',
+            val:7
+        },{
+            name:'鲸鱼_右',
+            fishType:' sever_right',
+            AniType:' ani_right_one',
+            val:7
+        },{
+            name:'鲸鱼_右',
+            fishType:' sever_right',
+            AniType:' ani_right_two',
+            val:7
+        },{
+            name:'仓鱼_左',
+            fishType:' six_left',
+            AniType:' ani_left_one',
+            val:6
+        },{
+            name:'仓鱼_左',
+            fishType:' six_left',
+            AniType:' ani_left_two',
+            val:6
+        },{
+            name:'仓鱼_右',
+            fishType:' six_right',
+            AniType:' ani_right_one',
+            val:6
+        },{
+            name:'仓鱼_右',
+            fishType:' six_right',
+            AniType:' ani_right_two',
+            val:6
+        }];
+
